3259ec02a95957448a6f5293202f20e31ae903d6
harunabdr <76208610+harunabdr@users.noreply.github.com>
2021-10-28 20:09:57 +0100

    US-005 Created client for TFL API

---
+++ Transport Planner/Models/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transport_Planner.Models
+{
+    public class ApiResponse
+    {
+        public string Query { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ Transport Planner/Services/TFLClientService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Transport_Planner.Models;
+
+namespace Transport_Planner.Services
+{
+    public class TFLClientService
+    {
+        public int GetLocations(string searchTerms)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("https://api.tfl.gov.uk");
+                    var response = client.GetAsync($"/Stoppoint/Search/{searchTerms}").Result;
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult =  response.Content.ReadAsStringAsync().Result;
+
+                    var rawData = JsonConvert.DeserializeObject<ApiResponse>(stringResult);
+
+                    return 0;
+                    
+
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    throw httpRequestException;
+                }
+            }
+        }
+            
+    }
+}
